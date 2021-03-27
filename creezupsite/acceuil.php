9e8c858b220077dbf3492ea032a90f25f7955be1
princedebaby <59696602+princedebaby@users.noreply.github.com>
2021-03-27 00:02:17 +0000

    Add files via upload

---
+++ creezupsite/acceuil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+ <link rel="stylesheet" href="styl.css">
+ 	<!-- Bootstrap Core CSS -->
+   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <title>Page d'accueil</title>
+
+</head>
+
+<body style="background-color:green;">
+
+         
+
+</BOdy>
+
+</HTml>
